4f91f2df72fcb2e35afa3517d53826b696c0733f
Ann Starovoyt <starrovoyt@gmail.com>
2017-01-28 00:35:11 +0300

    Initial Commit

---
+++ CCompiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CCompiler
+//
+//  Created by Анна Старовойт on 28.01.17.
+//  Copyright © 2017 Ann Starovoyt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
